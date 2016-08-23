cb4a2f60e4b44014e6b9f1bbbea9e7410495aea4
James Jester <elmojester@gmail.com>
2016-08-23 15:24:26 -0500

    Added Auth middleware

--- models/user.js
+++ models/user.js
@@ -86,6 +86,27 @@ module.exports = function(sequelize, DataTypes){
                             reject();
                         });
                 });
+            },
+            findByToken: function(token){
+                return new Promise(function(resolve, reject){
+                    try{
+                        var decodedJWT = jwt.verify(token, 'abc321');
+                        var bytes = cryptojs.AES.decrypt(decodedJWT.token, 'abc123');
+                        var tokenData = JSON.parse(bytes.toString(cryptojs.enc.Utf8));
+
+                        user.findById(tokenData.id).then(function(user){
+                            if(user){
+                                resolve(user);
+                            }else{
+                                reject();
+                            }
+                        }).catch(function(e){
+                            reject();
+                        });
+                    }catch(e){
+                        reject();
+                    }
+                });
             }
         }
     });
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
 var bcrypt = require('bcrypt');
+var middleware = require('./middleware')(db);
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -15,7 +16,7 @@ app.get('/', function(req, res){
     res.send('Todo API Root');
 });
 
-app.get('/todos',function(req, res){
+app.get('/todos', middleware.requireAuthentication, function(req, res){
     var query = req.query;
     var qTrue = query.hasOwnProperty('completed') && query.completed === 'true';
     var qFalse = query.hasOwnProperty('completed') && query.completed === 'false';
@@ -82,7 +83,7 @@ app.get('/todos',function(req, res){
     // res.json(filteredTodos);
 })
 
-app.get('/todos/:id',function(req, res){
+app.get('/todos/:id', middleware.requireAuthentication, function(req, res){
     var todoId = parseInt(req.params.id);
 
     db.todo.findById(todoId).then(function(todo){
@@ -103,7 +104,7 @@ app.get('/todos/:id',function(req, res){
     // }
 });
 
-app.post('/todos', function(req, res){
+app.post('/todos', middleware.requireAuthentication, function(req, res){
     var body = _.pick(req.body, 'description', 'completed');
 
     db.todo.create(body).then(function(todo){
@@ -113,7 +114,7 @@ app.post('/todos', function(req, res){
     })
 });
 
-app.delete('/todos/:id', function(req,res){
+app.delete('/todos/:id', middleware.requireAuthentication, function(req,res){
     var todoId = parseInt(req.params.id);
 
     db.todo.destroy({
@@ -155,7 +156,7 @@ app.delete('/todos/:id', function(req,res){
 
 });
 
-app.put('/todos/:id', function(req, res){
+app.put('/todos/:id', middleware.requireAuthentication, function(req, res){
     var todoId = parseInt(req.params.id);
     // var requestedItem = _.findWhere(todos, {id: todoId});
     var body = _.pick(req.body, 'description', 'completed');
